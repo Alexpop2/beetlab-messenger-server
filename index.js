29e63c7c54ce14bacade69f53885b7236f79a475
Alexei Poponin <alexqw1@ya.ru>
2019-03-13 00:51:51 +0300

    authorization refactoring && user register nickname realisation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const Authorization = require("./modules/authorization");
 const Messages = require("./modules/messages");
+const Users = require("./modules/users");
+const fs = require('fs');
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
@@ -13,9 +15,15 @@ const server = new grpc.Server();
 
 const authorization = new Authorization(server, protoLoader);
 const messages = new Messages(server, protoLoader);
+const users = new Users(server, protoLoader);
 
 // MARK: - Starting server
 
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+let credentials = grpc.ServerCredentials.createSsl(fs.readFileSync('./keys/ca.crt'), [{
+    cert_chain: fs.readFileSync('./keys/server.crt'),
+    private_key: fs.readFileSync('./keys/server.key')
+}], true);
+
+server.bind('0.0.0.0:50051', credentials);
 console.log('Server running at http://127.0.0.1:50051');
 server.start();
